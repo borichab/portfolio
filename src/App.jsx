b3e1d3a4fd8238a7ad3ccba3c2b86c9e3fd1b76c
borichab <borichab656@gmail.com>
2025-05-25 00:45:56 +0200

    Added Vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import Portfolio from './Portfolio'
+import { Analytics } from "@vercel/analytics/react" // Import Analytics for Vercel
 
 function App() {
   return (
     <>
+      <Analytics />
       <Portfolio />
     </>
   )
